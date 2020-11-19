93cb2f071a127f816c429fff710751a32fb3da77
hanaheyo <violets5618@gmail.com>
2020-11-19 15:19:50 +0900

    4. Progress Bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { AccordionItem } from './components/accordion/accordion-item.interface';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   public accordionItems: AccordionItem[] = [
@@ -19,4 +19,5 @@ export class AppComponent {
       isExpanded: false,
     }
   ]
+  public progressValue = 25;
 };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { CardComponent } from './components/card/card.component';
 import { AccordionComponent } from './components/accordion/accordion.component';
+import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { AccordionComponent } from './components/accordion/accordion.component';
     AppComponent,
     ToolbarComponent,
     CardComponent,
-    AccordionComponent
+    AccordionComponent,
+    ProgressBarComponent
   ],
   imports: [
     BrowserModule
