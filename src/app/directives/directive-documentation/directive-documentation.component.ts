59a74587ba5098f7412b9b7fc6699a580da4fcac
hanaheyo <violets5618@gmail.com>
2020-11-27 14:54:18 +0900

    18. Debounce Click Directive - fix

--- src/app/directives/directive-documentation/directive-documentation.component.ts
+++ src/app/directives/directive-documentation/directive-documentation.component.ts
@@ -1,33 +1,12 @@
-import { Component, EventEmitter, HostListener, OnDestroy, OnInit, Output } from '@angular/core';
-import { Subject, Subscription } from 'rxjs';
-import { debounceTime } from 'rxjs/internal/operators/debounceTime';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-directive-documentation',
   templateUrl: './directive-documentation.component.html',
   styleUrls: ['./directive-documentation.component.scss']
 })
-export class DirectiveDocumentationComponent implements OnInit, OnDestroy {
-  @Output() public debounceClick = new EventEmitter();
-  public clicks = new Subject();
-  public subscription: Subscription;
-
-  public ngOnInit(): void {
-    const debounceTimeInMs = 500;
-
-    this.subscription = this.clicks.pipe(
-      debounceTime(debounceTimeInMs)
-    ).subscribe((value: any) => this.debounceClick.emit(value));
-  }
-
-  public ngOnDestroy():void {
-    this.subscription.unsubscribe();
-  }
-
-  @HostListener('click', ['$event'])
-  public clickEvent(event: any): void {
-    event.preventDefault();
-    event.stopPropagation();
-    this.clicks.next();
+export class DirectiveDocumentationComponent {
+  public log() {
+    console.log('Hello World');
   }
 }
